f8c6df32dcb606aefb85d967876644ccdc138dfd
Marwan Ezzat Mohamed <marwan.ezzat.badawi@gmail.com>
2022-10-01 18:08:05 +0200

    Updates

---
+++ precache-manifest.bd38abb71f82e985e3f783cc9d66d075.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "305e4fa4fb382a0ff33e",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/css/main.3864a9f6.chunk.css"
+  },
+  {
+    "revision": "305e4fa4fb382a0ff33e",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/js/main.305e4fa4.chunk.js"
+  },
+  {
+    "revision": "68bbdb5f895d92f07d4d",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/js/1.68bbdb5f.chunk.js"
+  },
+  {
+    "revision": "53b7879a25dac6ed803c",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/js/runtime~main.53b7879a.js"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "64ce1edf917314788eef4032457e8a0b",
+    "url": "/My-Reads-A-Book-Tracking-App-Udacity/index.html"
+  }
+];
